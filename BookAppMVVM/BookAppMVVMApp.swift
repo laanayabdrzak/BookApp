a660862a674c7d9c0b9187e7957f8c2068355843
Abderrazak LAANAYA <laanayabdrzak@gmail.com>
2023-07-11 18:36:42 +0100

    Initial Commit

---
+++ BookAppMVVM/BookAppMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookAppMVVMApp.swift
+//  BookAppMVVM
+//
+//  Created by LAANAYA Abderrazak on 11/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct BookAppMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
